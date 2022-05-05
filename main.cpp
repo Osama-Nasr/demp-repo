798919ef8f367fb81bd1f3ad15de2c28e4f19d00
Osama-Nasr <osama.nasr22@gmail.com>
2022-05-05 15:53:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #inlude<iostream>
 using namespace std;
 
-int main(){}
+int main(){
+	cout << "testing" << endl;
+
+	for (int i = 0; i <4; i++)
+	{
+		cout << i << endl;
+	}
+
+
+	int p* = new int;
+	return 0;
+}
